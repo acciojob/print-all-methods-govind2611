264b136d6830b3d74dca90631ae92ca4da8f3f88
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-07-11 11:24:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-//your JS code here. If required.
 function allMethods() {
   //write your code here
-}
+	const allMethods = () => { return Object.getOwnPropertyNames(Math).filter(function (property)
+	{ return typeof Math[property] == "function";
+	}); 
+};
 
-alert(allMethods());
+ alert(allMethods());
+}
+	
